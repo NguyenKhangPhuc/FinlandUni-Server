368a3c08ae32d4e32c4fc70ffdefc30e40eadc8b
Khang Phuc Nguyen <nkpnkpnkp2005@gmail.com>
2025-04-14 07:43:55 +0300

    Route update: update route for fetching a university by a specific id

--- src/routes/universities.ts
+++ src/routes/universities.ts
@@ -10,6 +10,16 @@ universitiesRouter.get('/', async (_req, res) => {
     res.json(response);
 });
 
+universitiesRouter.get('/:id', async (req, res) => {
+    const id: string = req.params.id;
+    const response = await Universities.findOne({ id: id });
+    if (response) {
+        res.status(200).json(response);
+    } else {
+        res.status(404).json({ error: "malformed id" });
+    }
+});
+
 universitiesRouter.get('/:id/majors', async (req, res) => {
     const id: string = req.params.id;
     const response = await Majors.find({ uni_id: id });
